07bbc4e441e789d324a1bc7eb5f9ef23f4e5685b
ilya <dubovtsev98@gmail.com>
2017-07-08 17:39:41 +0400

    +models order & quest

--- ocean/settings.py
+++ ocean/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'order',
     'static_pages',
+    'quest',
 ]
 
 MIDDLEWARE = [
--- order/models.py
+++ order/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Order(models.Model):
+    team = models.CharField(max_length=200)
+    team_leader = models.CharField(max_length=200)
+    quest = models.ForeignKey('quest.Quest')
+    code = models.CharField(max_length=200)
+    email = models.EmailField()
+    vk = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    status = models.IntegerField()
+    date = models.DateField()
+    sent_date = models.DateField()
+
+    def send(self):
+        self.sent_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.team + ' (' + self.quest + ')'
+
